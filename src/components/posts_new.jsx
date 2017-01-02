21594fbb29b84b95510c6956355635e04daab2fc
schachte <rschacht@asu.edu>
2017-01-02 16:51:16 -0700

    update

--- src/components/posts_new.jsx
+++ src/components/posts_new.jsx
@@ -1,26 +1,33 @@
 import React, { Component } from 'react';
 import { reduxForm } from 'redux-form';
-
+import { createPost } from '../actions/index';
 
 class PostsNew extends Component {
 	render() {
+
+		const { fields: { title, categories, content }, handleSubmit } = this.props;
+		//Equivalent to: const title = this.props.fields.title
+
 		return (
 
-			<form>
+			<form onSubmit={handleSubmit(this.props.createPost)}>
 				<h3>Create New Post</h3>
 				<div className="form-group">
 					<label>Title</label>
-					<input type="text" className="form-control" />
+					<input type="text" className="form-control" { ...title } />
+					<div className="text-help">
+						{title.touched ? title.error : ''}
+					</div>					
 				</div>
 
 				<div className="form-group">
 					<label>Categories</label>
-					<input type="text" className="form-control" />
+					<input type="text" className="form-control" { ...categories } />
 				</div>
 
 				<div className="form-group">
 					<label>Content</label>
-					<textarea type="text" className="form-control" />
+					<textarea type="text" className="form-control" { ...content } />
 				</div>
 
 				<button type="submit" className="btn btn-primary">Submit</button>
@@ -29,7 +36,22 @@ class PostsNew extends Component {
 	}
 }
 
+function validate(values) {
+	const errors = {};
+
+	if (!values.title) {
+		errors.title = 'Enter blog title'
+	}
+
+	return errors;
+}
+
+
+//connect: first arg is mapStateToProps, second arg is mapDispatchToProps
+//redux-form: first arg is form config, second is mapState, 3rd is mapDispatch
+
 export default reduxForm({
 	form: 'PostsNewForm',
-	fields: ['title', 'categories', 'content']
-})(PostsNew);
+	fields: ['title', 'categories', 'content'],
+	validate
+}, null, { createPost })(PostsNew);
