c967a6c23998f99e90aff012cb581c4766599685
schachte <rschacht@asu.edu>
2017-01-02 17:07:03 -0700

    update

--- src/components/posts_new.jsx
+++ src/components/posts_new.jsx
@@ -1,8 +1,19 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import { reduxForm } from 'redux-form';
 import { createPost } from '../actions/index';
+import { Link } from 'react-router';
+
 
 class PostsNew extends Component {
+
+	static contextTypes = {
+		router: PropTypes.object
+	}
+
+	onSubmit(props) {
+		this.props.createPost(props);
+	}
+
 	render() {
 
 		const { fields: { title, categories, content }, handleSubmit } = this.props;
@@ -10,9 +21,9 @@ class PostsNew extends Component {
 
 		return (
 
-			<form onSubmit={handleSubmit(this.props.createPost)}>
+			<form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
 				<h3>Create New Post</h3>
-				<div className="form-group">
+				<div className={`form-group ${title.touched && title.invalid ? 'has-danger' : '' || title.touched && !title.invalid ? 'has-success' : ''}`}>
 					<label>Title</label>
 					<input type="text" className="form-control" { ...title } />
 					<div className="text-help">
@@ -20,17 +31,25 @@ class PostsNew extends Component {
 					</div>					
 				</div>
 
-				<div className="form-group">
+				<div className={`form-group ${categories.touched && categories.invalid ? 'has-danger' : ''}`}>
 					<label>Categories</label>
 					<input type="text" className="form-control" { ...categories } />
+					<div className="text-help">
+						{categories.touched ? categories.error : ''}
+					</div>					
 				</div>
 
-				<div className="form-group">
+				<div className={`form-group ${content.touched && content.invalid ? 'has-danger' : ''}`}>
 					<label>Content</label>
 					<textarea type="text" className="form-control" { ...content } />
+					<div className="text-help">
+						{content.touched ? content.error : ''}
+					</div>	
 				</div>
 
 				<button type="submit" className="btn btn-primary">Submit</button>
+				<Link to='/' className='btn btn-danger'>Cancel</Link>
+
 			</form>
 		);
 	}
@@ -40,8 +59,17 @@ function validate(values) {
 	const errors = {};
 
 	if (!values.title) {
-		errors.title = 'Enter blog title'
+		errors.title = 'Enter blog title';
 	}
+
+	if (!values.categories) {
+		errors.categories = 'Enter set(s) of categories';
+	}
+
+	if (!values.content) {
+		errors.content = 'Enter content';
+	}
+
 
 	return errors;
 }
